d6ece31e6291be60894d6235244b682fa3103431
DIEM01 <77025259+DIEM01@users.noreply.github.com>
2021-06-16 14:43:58 -0500

    Add files via upload

---
+++ C/holamundo.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <math.h>
+int main (){
+	
+	float x,y,z, hipo;
+	
+	
+	printf ("dame la base 1 ");
+	scanf ("%f",&x);
+	printf ("dame la base 2  ");
+	scanf ("%f",&y);
+	printf ("dame la altura  ");
+	scanf ("%f",&z);
+	hipo = (x + y)*(z)/2;
+
+	
+	
+	printf ("la suma, resta, multiplicacion, divicion de dos numeros es: %f ", hipo);
+	
+	
+	
+	
+	return 0;
+
+}

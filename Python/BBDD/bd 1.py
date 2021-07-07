fb284cfa96bc0c9054a045c8d5e5c860fbe2b724
DIEM01 <ivanfloresmoranxd@gmail.com>
2021-07-07 15:55:30 -0500

    1 BD

---
+++ Python/BBDD/bd 1.py
@@ -0,0 +1,10 @@
+import sqlite3
+conexion = sqlite3.connect ("BD")
+#
+
+cursor = conexion. cursor ()
+cursor.execute ("CREATE TABLE PRODUCTOS (NOMBRE_ART varchar (50), PRECIO INTEGER , SECCION varchar (20) )") #solo se hace 1 vez 
+
+cursor.execute ("INSERT INTO Productos VALUES ('BALON', ' 15', 'DEPORTES')")
+conexion.commit ()
+conexion.close ()

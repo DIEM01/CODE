9de5b14643f50cf714b8d385b18783befc67274b
DIEM01 <ivanfloresmoranxd@gmail.com>
2021-07-07 15:59:28 -0500

    BD prueba

--- Python/BBDD/bd 1.py
+++ Python/BBDD/bd 1.py
@@ -3,7 +3,7 @@ conexion = sqlite3.connect ("BD")
 #
 
 cursor = conexion. cursor ()
-cursor.execute ("CREATE TABLE PRODUCTOS (NOMBRE_ART varchar (50), PRECIO INTEGER , SECCION varchar (20) )") #solo se hace 1 vez 
+#cursor.execute ("CREATE TABLE PRODUCTOS (NOMBRE_ART varchar (50), PRECIO INTEGER , SECCION varchar (20) )") #solo se hace 1 vez 
 
 cursor.execute ("INSERT INTO Productos VALUES ('BALON', ' 15', 'DEPORTES')")
 conexion.commit ()

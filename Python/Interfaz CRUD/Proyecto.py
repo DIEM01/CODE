f5b47eb938e21d3d0ad594cbcbefd0f634cc076d
DIEM01 <ivanfloresmoranxd@gmail.com>
2021-07-12 19:14:38 -0500

    lable and boton

--- Python/Interfaz CRUD/Proyecto.py
+++ Python/Interfaz CRUD/Proyecto.py
@@ -1,40 +1,50 @@
 from tkinter import *
+import sqlite3 
 raiz = Tk()
-myframe = Frame(raiz, width = 100, height = 180)
-raiz.geometry("350x400")
+myframe = Frame(raiz, width = 800, height = 300)
+raiz.geometry("300x270")
 raiz.resizable(0,0)
 myframe.pack()
 raiz.title("CRUD PERSONAS")
 cuadroaclasificar = StringVar()
 milable1 = Label(myframe , text = "Registros")
-milable1.grid(row = 1,column = 1)
-milable2 = Label(myframe , text = "           ID : ")
-milable2.grid(row = 2,column = 1)
-milable3 = Label(myframe , text="Nombre : ")
-milable3.grid(row = 3,column = 1)
-milable4 = Label(myframe , text = "     Apellido Paterno : ")
-milable4.grid(row=  4,column = 1)
-milable5 = Label(myframe , text = "    Apellido materno      ")
-milable5.grid(row=  5,column = 1)
-milable6 = Label(myframe , text = "      Contraseña  ")
-milable6.grid(row=  6,column = 1)
-milable7 = Label(myframe , text = "      Dirección   ")
-milable7.grid(row=  7,column = 1)
+milable1.place(x = 5, y = 5)
+
+milable2 = Label(myframe , text = "ID:")
+milable2.place(x = 107, y = 30)
 
 
+milable3 = Label(myframe , text="Nombre:")
+milable3.place(x = 75, y = 60)
+
+milable4 = Label(myframe , text = "Apellido Paterno:")
+milable4.place(x = 32, y = 90)
+milable5 = Label(myframe , text = "Apellido Materno:")
+milable5.place(x = 29, y = 120)
+milable6 = Label(myframe , text = "Contraseña:")
+milable6.place(x = 62, y = 150)
+milable7 = Label(myframe , text = "Dirección:")
+milable7.place(x = 72, y = 180)
+
+"""
 cuadroaclasificar = Entry(myframe, textvariable=cuadroaclasificar )
 cuadroaclasificar.grid(row = 5,column=2)
-cuadroaclasificar.config (justify = "right")
+cuadroaclasificar.config (justify = "right")"""
+
+
 def window (): 
        raiz.destroy() 
 def borrar():
     res = ""
     milable4.config(text = res)
-    ''''
-boton1 =Button(myframe, text = "Reiniciar", width = 10,command=borrar)
-boton1.grid(row = 8, column = 1)
-boton2 =Button(myframe, text = "Clasificar", width = 10)
-boton2.grid(row = 6, column = 1)
-boton3 =Button(myframe, text = "Salir", width = 10, command=window)
-boton3.grid(row = 8, column = 2)'''
+    
+boton1 =Button(myframe, text = "Create", width = 6)
+boton1.place(x = 10, y = 240)
+boton2 =Button(myframe, text = "Read", width = 6)
+boton2.place(x = 80, y = 240)
+boton3 =Button(myframe, text = "Update", width = 6)
+boton3.place(x = 150, y = 240)
+boton4 =Button(myframe, text = "Delete", width = 6)
+boton4.place(x = 220, y = 240)
+
 raiz.mainloop() 

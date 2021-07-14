6ac94bfd5f367005b5e953a8b4d26a12bbed1585
DIEM01 <ivanfloresmoranxd@gmail.com>
2021-07-13 19:22:00 -0500

    Interfaz 2.0

--- Python/Interfaz CRUD/Proyecto.py
+++ Python/Interfaz CRUD/Proyecto.py
@@ -1,50 +1,106 @@
 from tkinter import *
+import tkinter as tk
 import sqlite3 
 raiz = Tk()
 myframe = Frame(raiz, width = 800, height = 300)
-raiz.geometry("300x270")
+raiz.geometry("280x210")
 raiz.resizable(0,0)
+myframe['bg'] = '#49A'
+raiz['bg'] = '#49A'
 myframe.pack()
 raiz.title("CRUD PERSONAS")
-cuadroaclasificar = StringVar()
-milable1 = Label(myframe , text = "Registros")
-milable1.place(x = 5, y = 5)
-
-milable2 = Label(myframe , text = "ID:")
-milable2.place(x = 107, y = 30)
 
 
-milable3 = Label(myframe , text="Nombre:")
-milable3.place(x = 75, y = 60)
 
-milable4 = Label(myframe , text = "Apellido Paterno:")
-milable4.place(x = 32, y = 90)
-milable5 = Label(myframe , text = "Apellido Materno:")
-milable5.place(x = 29, y = 120)
-milable6 = Label(myframe , text = "Contraseña:")
-milable6.place(x = 62, y = 150)
-milable7 = Label(myframe , text = "Dirección:")
-milable7.place(x = 72, y = 180)
 
-"""
-cuadroaclasificar = Entry(myframe, textvariable=cuadroaclasificar )
-cuadroaclasificar.grid(row = 5,column=2)
-cuadroaclasificar.config (justify = "right")"""
+
+
+Mymenu = tk.Menu(raiz)  
+filemenu = tk.Menu(Mymenu, tearoff=0) 
+Mymenu.add_cascade(label='BBDD', menu=filemenu)
+filemenu.add_command(label='Crear')
+filemenu.add_separator()
+filemenu.add_command(label='Salir')
+raiz.config(menu=Mymenu)  
+
+
+editmenu = tk.Menu(Mymenu, tearoff=0)   
+Mymenu.add_cascade(label='CRUD', menu=editmenu)   
+editmenu.add_command(label='Crear')
+editmenu.add_command(label='Leer')
+editmenu.add_command(label='Actualizar')
+editmenu.add_command(label='Eliminar')
+
+editmenu = tk.Menu(Mymenu, tearoff=0)   
+Mymenu.add_cascade(label='Edición', menu=editmenu)   
+editmenu.add_command(label='Borrar datos')
+
+editmenu = tk.Menu(Mymenu, tearoff=0)   
+Mymenu.add_cascade(label='Acerca de', menu=editmenu)   
+editmenu.add_command(label='Licencia ')
+
+
+
+ca = StringVar()
+
+m2 = Label(myframe , text = "ID:")
+m2.place(x = 107, y = 10)
+m2.config (bg ='#49A',fg ='white')
+m3 = Label(myframe , text="Nombre:")
+m3.place(x = 75, y = 40)
+m3.config (bg ='#49A',fg ='white')
+m4 = Label(myframe , text = "Apellido Paterno:")
+m4.place(x = 32, y = 70)
+m4.config (bg ='#49A',fg ='white')
+m5 = Label(myframe , text = "Apellido Materno:")
+m5.place(x = 29, y = 100)
+m5.config (bg ='#49A',fg ='white')
+m6 = Label(myframe , text = "Contraseña:")
+m6.place(x = 62, y = 130)
+m6.config (bg ='#49A',fg ='white')
+
+
+ca = Entry(myframe, textvariable=ca )
+ca.place(x = 140, y = 10)
+
+c1 = Entry(myframe, textvariable=ca )
+c1.place(x = 140, y = 40)
+
+c2 = Entry(myframe, textvariable=ca )
+c2.place(x = 140, y = 70)
+
+c3 = Entry(myframe, textvariable=ca )
+c3.place(x = 140, y = 100)
+
+c4 = Entry(myframe, textvariable=ca )
+c4.place(x = 140, y = 130)
+
+
+
+
+
+
+
+
 
 
 def window (): 
+
        raiz.destroy() 
 def borrar():
     res = ""
-    milable4.config(text = res)
+    m4.config(text = res)
     
-boton1 =Button(myframe, text = "Create", width = 6)
-boton1.place(x = 10, y = 240)
-boton2 =Button(myframe, text = "Read", width = 6)
-boton2.place(x = 80, y = 240)
-boton3 =Button(myframe, text = "Update", width = 6)
-boton3.place(x = 150, y = 240)
-boton4 =Button(myframe, text = "Delete", width = 6)
-boton4.place(x = 220, y = 240)
+b1 =Button(myframe, text = "Create", width = 6)
+b1.place(x = 10, y = 170)
+
+b2 =Button(myframe, text = "Read", width = 6)
+b2.place(x = 80, y = 170)
+
+b3 =Button(myframe, text = "Update", width = 6)
+b3.place(x = 150, y = 170)
+
+b4 =Button(myframe, text = "Delete", width = 6)
+b4.place(x = 220, y = 170)
 
 raiz.mainloop() 
